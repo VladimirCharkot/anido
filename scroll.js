8606a2eeefb279a816f6f2d8b342907c48d5c100
VladimirCharkot <vlad.chk@gmail.com>
2020-05-24 00:25:50 -0300

    Emprolijamiento

--- scroll.js
+++ scroll.js
@@ -3,24 +3,27 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.documentElement.scrollTop < 20){
-        document.getElementById("cabecera_cont").style.padding = "50px 9%";
-        document.getElementById("titulo").style.fontSize = "42px";
-        document.getElementById("nav").style.height = "40px";
+        document.getElementById("titulo").style.fontSize = "3rem";
+        document.getElementById("titular").style.height = "150px";
+        
+        document.getElementById("pag").gridTemplateRows = "150px 5% auto";
         document.getElementById("img_nido").style.height = "120px"
+        document.getElementById("img_nido").style.width = "150px";
+        document.getElementsByClassName("nav")[0].style.height = "50px";
         var btns = document.getElementsByClassName("nav_btn");
         for (i in btns) if (btns[i].style) btns[i].style.fontSize = "12px";
+        console.log("FAFAFAF");
     }
     if (document.documentElement.scrollTop > 20 && document.documentElement.scrollTop < 120){
-        document.getElementById("cabecera_cont").style.padding = 70-document.documentElement.scrollTop + "px 9%";
         document.getElementById("img_nido").style.height = "0px"
     }
     
     if (document.documentElement.scrollTop > 120) {
-        document.getElementById("cabecera_cont").style.padding = "5px";
-        document.getElementById("nav").style.height = "0px"
-        document.getElementById("titulo").style.fontSize = "22px";
-        document.getElementById("titulo").style.textAlign = "left";
+        document.getElementById("titulo").style.fontSize = "1.3rem";
+        document.getElementById("titular").style.height = "50px";
         document.getElementById("img_nido").style.height = "0px";
+        document.getElementById("img_nido").style.width = "30px";
+        document.getElementsByClassName("nav")[0].style.height = "0px";
         var btns = document.getElementsByClassName("nav_btn");
         for (i in btns) if (btns[i].style) btns[i].style.fontSize = "0px";
         
