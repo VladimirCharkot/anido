8ea2291328432189aa7c59b140b300a328341b8c
VladimirCharkot <vlad.chk@gmail.com>
2020-05-25 16:11:33 -0300

    Referencias, comillas y otros detalles

--- scroll.js
+++ scroll.js
@@ -10,7 +10,7 @@ window.original_nav_height = document.getElementsByClassName("nav")[0].style.hei
 
 
 function scrollFunction() {
-    if (document.documentElement.scrollTop < 20){
+    if (document.documentElement.scrollTop < 40){
         document.getElementById("titulo").style.transform = window.original_titulo_transf;
         document.getElementById("titulo").style.fontSize = window.original_titulo_fontsize;
         document.getElementById("titular").style.height = window.original_titular_fontsize;
@@ -22,12 +22,7 @@ function scrollFunction() {
         var btns = document.getElementsByClassName("nav_btn");
         for (i in btns) if (btns[i].style) btns[i].style.fontSize = "1em";
     }
-    if (document.documentElement.scrollTop > 20 && document.documentElement.scrollTop < 120){
-        document.getElementById("img_nido").style.height = "0px"
-    }
-    
-    if (document.documentElement.scrollTop > 120) {
-        console.log("Corriendo");
+    if (document.documentElement.scrollTop > 40) {
         document.getElementById("titulo").style.transform = "translateY(0)";
         document.getElementById("titulo").style.fontSize = "1rem";
         document.getElementById("titular").style.height = "50px";
