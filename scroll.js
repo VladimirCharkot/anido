fc8ead0652dec29ddc3c15c00dac5c1255fe3ec7
VladimirCharkot <vlad.chk@gmail.com>
2020-06-13 00:58:59 -0300

    PDFs, menues desplegables y refactor

--- scroll.js
+++ scroll.js
@@ -1,35 +1,41 @@
 
 window.onscroll = function() {scrollFunction()};
 
-window.original_titulo_fontsize = document.getElementById("titulo").style.fontSize;
-window.original_titulo_transf = document.getElementById("titulo").style.transform;
-window.original_titular_fontsize = document.getElementById("titular").style.height;
-window.original_nidito_height = document.getElementById("img_nido").style.height;
-window.original_nidito_width = document.getElementById("img_nido").style.width
-window.original_nav_height = document.getElementsByClassName("nav")[0].style.height;
-window.original_btn_fontsize = document.getElementsByClassName("nav_btn")[0].style.fontSize;
+var titular = document.querySelector("#titular");
+var titulo = document.querySelector("#titular h1");
+var nidito = document.querySelector("#nidito");
+var nav = document.querySelector("#nav");
+var nav_btn = document.querySelector(".nav_btn");
+
+window.original_titular_fontsize    = titular.style.height;
+window.original_titulo_fontsize     = titulo.style.fontSize;
+window.original_titulo_transf       = titulo.style.transform;
+window.original_nidito_height       = nidito.style.height;
+window.original_nidito_width        = nidito.style.width
+window.original_nav_height          = nav.style.height;
+window.original_btn_fontsize        = nav_btn.style.fontSize;
 
 function scrollFunction() {
     if (document.documentElement.scrollTop < 40){
-        document.getElementById("titulo").style.transform = window.original_titulo_transf;
-        document.getElementById("titulo").style.fontSize = window.original_titulo_fontsize;
-        document.getElementById("titular").style.height = window.original_titular_fontsize;
+        titulo.style.transform = window.original_titulo_transf;
+        titulo.style.fontSize = window.original_titulo_fontsize;
+        titular.style.height = window.original_titular_fontsize;
         
-        document.getElementById("img_nido").style.height = window.original_nidito_height;
-        document.getElementById("img_nido").style.width = window.original_nidito_width;
+        nidito.style.height = window.original_nidito_height;
+        nidito.style.width = window.original_nidito_width;
         
-        document.getElementsByClassName("nav")[0].style.height = window.original_nav_height;
-        var btns = document.getElementsByClassName("nav_btn");
+        nav.style.height = window.original_nav_height;
+        var btns = document.querySelectorAll(".nav_btn");
         for (i in btns) if (btns[i].style) btns[i].style.fontSize = window.original_btn_fontsize;
     }
     if (document.documentElement.scrollTop > 40) {
-        document.getElementById("titulo").style.transform = "translateY(0)";
-        document.getElementById("titulo").style.fontSize = "1rem";
-        document.getElementById("titular").style.height = "50px";
-        document.getElementById("img_nido").style.height = "0px";
-        document.getElementById("img_nido").style.width = "30px";
-        document.getElementsByClassName("nav")[0].style.height = "0px";
-        var btns = document.getElementsByClassName("nav_btn");
+        titulo.style.transform = "translateY(0)";
+        titulo.style.fontSize = "1rem";
+        titular.style.height = "50px";
+        nidito.style.height = "0px";
+        nidito.style.width = "30px";
+        nav.style.height = "0px";
+        var btns = document.querySelectorAll(".nav_btn");
         for (i in btns) if (btns[i].style) btns[i].style.fontSize = "0em";
         
     }
--- tooltip.js
+++
@@ -1,9 +0,0 @@
-
-var notas = ['Esta Convención tiene la característica de organizarse y desarrollarse con una propuesta de formación. Es decir, además de contemplarla como espacio de encuentro, socialización, libre intercambio y presentaciones escénicas con malabares, el centro de la propuesta es la cantidad de horas dadas a la formación mediante talleres y seminarios. Gonzalo Borgogno (Cuete) dice en la entrevista: «realizamos el evento a partir de visualizar la necesidad de los malabaristas de que haya un espacio para profundizar lo educativo».']
-
-var quotes = '«»'
-
-function tooltip(n){
-    console.log("Tooltip " + n);
-    
-}
